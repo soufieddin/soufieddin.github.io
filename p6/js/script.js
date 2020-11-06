d9a2c3de20cb37469ae60ca344a853bb08a97177
soufieddin <eddin2209@gmail.com>
2020-11-06 14:57:49 +0100

    update2

--- p6/js/script.js
+++ p6/js/script.js
@@ -97,19 +97,19 @@ const app = {
     const d = new Date(ms);
     let day = d.getDay();
     if (day == 0) {
-      return "Zondag";
+      return "Sunday";
     } else if (day == 1) {
-      return "Maandag";
+      return "Monday";
     } else if (day == 2) {
-      return "Dinsdag";
+      return "Tuesday";
     } else if (day == 3) {
-      return "Woensdag";
+      return "ednesday";
     } else if (day == 4) {
-      return "Donderdag";
+      return "Thursday";
     } else if (day == 5) {
-      return "Vrijdag";
+      return "Friday";
     } else if (day == 6) {
-      return "Zaterdag";
+      return "Saturday";
     }
   },
   generateCountdown: function() {
