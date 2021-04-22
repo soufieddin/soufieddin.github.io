2c986f82bf94e9bc2b6b3a0dc85835608be00f5f
Marina Soufi <xysamemir@gmail.com>
2021-04-22 21:51:36 +0200

    update

--- .eleventy.js
+++ .eleventy.js
@@ -46,7 +46,7 @@ module.exports = function (eleventyConfig) {
       includes: "_includes",
       data: "_data",
     },
-    pathPrefix: "digital-agency-digital-agency",
+    pathPrefix: "/11tyio-boilerplate/inova",
     templateFormats: ["html", "liquid", "md", "njk"],
     markdownTemplateEngine: "liquid",
     htmlTemplateEngine: "njk",
