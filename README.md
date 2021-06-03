c7aa4b16541d2e28afda009a1283d03710e1ff16
soufieddin <eddin2209@gmail.com>
2021-06-03 10:54:12 +0200

    update

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# 11tyio-boilerplate
-11tyio Bolerplate | Associate Degree in Computer Programming | Artevelde University of Applied Sciences
-Team:
-Maryna Soufi
-Aiden Soufi
-
-test
-
+Portfolio website of Aiden Soufi 
--- site/_static/js/main.js
+++ site/_static/js/main.js
@@ -1,18 +1,14 @@
-const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoibWFycnlzIiwiYSI6ImNrbGdsdWF6azA0c2Iyb3Jydm9xcG1qMzYifQ.PTJcG6qfqXf1uK1F43FrSA"; // fill in your personal mapbox acces token here
-const LATITUDE = 51.028810;
-const LONGTITUDE = 3.799770;
+
 (() => {
   const app = {
     initialize () {
       this.cacheElements();
       this.registerEventListeners();
-      if(this.map) {
-        this.showMap ();
-      }
+
       this.showModalWindow();
     },
     cacheElements () {
-      this.map = document.getElementById('map');
+      
       this.btnToTopElement = document.querySelector('.button--toTop');
       this.myModal = document.getElementById('myModal');
       this.myInput = document.getElementById('myInput');
@@ -28,18 +24,7 @@ const LONGTITUDE = 3.799770;
         });
       }
     },
-    showMap () {
-      mapboxgl.accessToken = 'pk.eyJ1IjoibWFycnlzIiwiYSI6ImNrbGdsdWF6azA0c2Iyb3Jydm9xcG1qMzYifQ.PTJcG6qfqXf1uK1F43FrSA';
-      var map = new mapboxgl.Map({
-      container: 'map', // container id
-      style: 'mapbox://styles/mapbox/streets-v11', // style URL
-      center: [3.799770,  51.028810], // starting position [lng, lat]
-      zoom: 9 // starting zoom
-      });
-      var marker = new mapboxgl.Marker()
-      .setLngLat([3.799770, 51.028810])
-      .addTo(map);
-    },
+    
     showModalWindow() {
       const burger = document.querySelector('.button--burger');
       const modalWindow = document.querySelector('.my-modal');
